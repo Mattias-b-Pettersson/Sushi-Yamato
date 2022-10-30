62d0bf28b085b7c08f240bc003a444ba56d0c1e0
Mattias-b-Pettersson <mattep3@gmail.com>
2022-10-30 15:33:50 +0000

    Add the booking model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+TABLESIZE_CHOISES = (
+    ("2", "2"),
+    ("4", "4"),
+    ("6", "6"),
+    ("8", "8"),
+)
+
+
+class Booking(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    phonenumber = models.IntegerField()
+    email = models.EmailField(max_length=200)
+    date = models.DateField()
+    time = models.TimeField()
+    tablesize = models.CharField(max_length=20, choices=TABLESIZE_CHOISES,
+                                 default='2')
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
