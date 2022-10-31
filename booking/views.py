f32e6a0da66b99217daed0d1f963981d36a16488
Mattias-b-Pettersson <mattep3@gmail.com>
2022-10-31 20:40:58 +0000

    Add booking view and url

--- booking/views.py
+++ booking/views.py
@@ -7,14 +7,24 @@ from .forms import BookingForm
 
 # Create your views here.
 def booking(request):
+    form = BookingForm(request.POST)
     if request.method == "POST":
-        form = BookingForm(request.POST)
+        if request.POST.get('booking-number', ''):
+            booking_no = request.POST.get('booking-number', '')
+            print(request.POST)
+            if Booking.objects.filter(booking_no=booking_no).exists():
+                if request.POST.get("action") == "viewbook":
+                    print("retur§")
+                    return redirect(f"view-booking/{booking_no}")
+                elif request.POST.get("action") == "editbook":
+                    print("retur")
+                    return redirect(f"edit-booking/{booking_no}")
         if form.is_valid():
             form.save()
-            return redirect("/")
+            return redirect("booking-confirmation")
     else:
         form = BookingForm()
         context = {
             "form": form
         }
-        return render(request, "form.html", context)
+        return render(request, "book.html", context)
