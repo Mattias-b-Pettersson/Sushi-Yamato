b3b07a906d1ac66200b7c3a57de41574630d2e80
Mattias-b-Pettersson <mattep3@gmail.com>
2022-10-31 13:31:08 +0000

    Add unique id generator for booking number

--- booking/models.py
+++ booking/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+import uuid
 
 TABLESIZE_CHOISES = (
     ("2", "2"),
@@ -9,9 +10,19 @@ TABLESIZE_CHOISES = (
 )
 
 
+def unique_uuid():
+    # Generate ID once, then check the db. If id exists, keep trying.
+    temp_booking_no = uuid.uuid4().hex[:10]
+    while Booking.objects.filter(booking_no=temp_booking_no).exists():
+        temp_booking_no = uuid.uuid4().hex[:10]
+    return temp_booking_no
+
+    
+
 class Booking(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
+    booking_no = models.CharField(unique=True, default=unique_uuid, max_length=10, editable=False)
     slug = models.SlugField(max_length=200, unique=True)
     phonenumber = PhoneNumberField(blank=True)
     email = models.EmailField(max_length=200)
@@ -21,4 +32,4 @@ class Booking(models.Model):
                                  default='2')
 
     def __str__(self):
-        return f"{self.firstname} {self.lastname}"
+        return f"{self.firstname} {self.lastname}, {self.booking_no}"
