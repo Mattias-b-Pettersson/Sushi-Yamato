bbdb6bf9d96a4726367aec26ac8e9abdccd398f1
Mattias-b-Pettersson <mattep3@gmail.com>
2022-11-02 20:57:05 +0000

    Add input type in date and time field in book form

--- booking/forms.py
+++ booking/forms.py
@@ -3,7 +3,16 @@ from .models import Booking
 from phonenumber_field.formfields import PhoneNumberField
 
 
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
+class TimeInput(forms.TimeInput):
+    input_type = "time"
+
+
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ("firstname", "lastname", "email", "phonenumber", "date", "time", "tablesize")
+        widgets = {"date": DateInput(), "time": TimeInput()}
