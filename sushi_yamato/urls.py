4574e7a48b14e871bf53a64c2523fa65f5f2afe3
Mattias-b-Pettersson <mattep3@gmail.com>
2022-11-06 21:04:17 +0000

    Add urls to menu

--- sushi_yamato/urls.py
+++ sushi_yamato/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.Home.as_view(), name="home"),
     path("booking/", include("booking.urls")),
+    path("menu/", include("menu.urls")),
     path('accounts/', include('allauth.urls')),
 ]
