1f3c7da0fa49b9efbcfffdfa5e1b575c389f58e9
Mattias-b-Pettersson <mattep3@gmail.com>
2022-11-06 22:23:53 +0000

    Add delete menu item buttons

--- menu/urls.py
+++ menu/urls.py
@@ -3,5 +3,6 @@ from menu import views
 
 urlpatterns = [
     path("", views.MenuView.as_view(), name="menu"),
-    
+    path("delete_food_item/<slug>", views.DeleteFoodItem.as_view(), name="delete_food_item"),
+    path("delete_drink_item/<slug>", views.DeleteDrinkItem.as_view(), name="delete_drink_item")
 ]
