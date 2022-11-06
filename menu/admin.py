a7aef4e37542bdbc4a83e20ec502f03e24ded88c
Mattias-b-Pettersson <mattep3@gmail.com>
2022-11-06 12:48:31 +0000

    Add fooditem and drinkitems to admin panel

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import DrinkItem, FoodItem
 
-# Register your models here.
+
+@admin.register(FoodItem)
+class FoodAdmin(admin.ModelAdmin):
+    search_fields = ["name", "price", "type", "description"]
+    list_display = ["name", "price", "type", "description"]
+    list_filter = ("name", "price", "type")
+
+@admin.register(DrinkItem)
+class DrinkAdmin(admin.ModelAdmin):
+    search_fields = ["name", "price", "type", "description"]
+    list_display = ["name", "price", "type", "description"]
+    list_filter = ("type",)
