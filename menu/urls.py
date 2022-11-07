7e8cf3ef8354996b2031ecbc6939494ae9376fa7
Mattias-b-Pettersson <mattep3@gmail.com>
2022-11-07 21:39:09 +0000

    Add EditMenuItem view

--- menu/urls.py
+++ menu/urls.py
@@ -4,5 +4,6 @@ from menu import views
 urlpatterns = [
     path("", views.MenuView.as_view(), name="menu"),
     path("delete_food_item/<slug>", views.DeleteFoodItem.as_view(), name="delete_food_item"),
-    path("delete_drink_item/<slug>", views.DeleteDrinkItem.as_view(), name="delete_drink_item")
+    path("delete_drink_item/<slug>", views.DeleteDrinkItem.as_view(), name="delete_drink_item"),
+    path("edit_menu_item/<slug>", views.EditMenuItem.as_view(), name="edit_menu_item"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .models import FoodItem, DrinkItem 
+from .forms import FoodItemForm, DrinkItemForm
 
 
 class MenuView(View):
@@ -50,3 +51,70 @@ class DeleteDrinkItem(View):
         messages.success(request, "Menu item deleted!")
         return redirect(reverse("menu"))
 
+
+class EditMenuItem(View):
+    def post(self, request, slug):
+        if DrinkItem.objects.filter(slug=slug).exists():
+            drink_item = get_object_or_404(DrinkItem, slug=slug)
+            form = DrinkItemForm(request.POST, instance=drink_item)
+            filledform = DrinkItemForm(instance=drink_item)
+            context = {
+                    "active": "menu",
+                    "form": filledform,
+                }
+
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Update successfull!")
+                return render(request, "menu.html", context)
+
+            if not form.is_valid():
+                messages.error(request, "Update was not successfull!")
+                return render(request, "edit-menu.html", context)
+
+        if FoodItem.objects.filter(slug=slug).exists():
+            food_item = get_object_or_404(FoodItem, slug=slug)
+            form = FoodItemForm(request.POST, instance=food_item)
+            filledform = FoodItemForm(instance=food_item)
+            context = {
+                    "active": "menu",
+                    "form": filledform,
+                }
+
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Update successfull!")
+                return render(request, "menu.html", context)
+
+            if not form.is_valid():
+                messages.error(request, "Update was not successfull!")
+                return render(request, "edit-menu.html", context)
+
+
+        else:
+            messages.error(request, "Menu item was not found!")
+            return redirect(reverse("menu"))
+
+    def get(self, request, slug):
+        if DrinkItem.objects.filter(slug=slug).exists():
+            drink_item = get_object_or_404(DrinkItem(), slug=slug)
+            drink_form = DrinkItemForm(instance=drink_item)
+            context = {
+                "active": "menu",
+                "form": drink_form,
+            }
+            return render(request, "edit-menu.html", context)
+
+        elif FoodItem.objects.filter(slug=slug).exists():
+            food_item = get_object_or_404(FoodItem, slug=slug)
+            food_form = FoodItemForm(instance=food_item)
+            context = {
+                "active": "menu",
+                "form": food_form,
+            }
+
+            return render(request, "edit-menu.html", context)
+
+        else:
+            messages.error(request, "Menu item was not found!")
+            return redirect(reverse("menu"))
