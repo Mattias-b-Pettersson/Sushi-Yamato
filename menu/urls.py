aea07f12a8608feeaabebdd179d4e1e6cfdd57a3
Mattias-b-Pettersson <mattep3@gmail.com>
2022-11-07 22:30:03 +0000

    Add urls to "add menu item" page

--- menu/urls.py
+++ menu/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("delete_food_item/<slug>", views.DeleteFoodItem.as_view(), name="delete_food_item"),
     path("delete_drink_item/<slug>", views.DeleteDrinkItem.as_view(), name="delete_drink_item"),
     path("edit_menu_item/<slug>", views.EditMenuItem.as_view(), name="edit_menu_item"),
+    path("add_drink_item", views.AddDrinkItem.as_view(), name="add_drink_item"),
+    path("add_food_item", views.AddFoodItem.as_view(), name="add_food_item"),
+
 ]
