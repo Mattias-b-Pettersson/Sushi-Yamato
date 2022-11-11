29e53126847d4c2137cbf01cd7410a7fed4a8193
Mattias-b-Pettersson <mattep3@gmail.com>
2022-11-11 16:46:42 +0000

    Fix PEP8 warnings, line too long and no new line

--- booking/urls.py
+++ booking/urls.py
@@ -4,8 +4,14 @@ from booking import views
 urlpatterns = [
     path("", views.BookingView.as_view(), name="book"),
     path("open/", views.BookingSearch.as_view(), name="open_booking"),
-    path("edit/<booking_no>", views.BookingEdit.as_view(), name="edit_booking"),
-    path("delete/<booking_no>", views.DeleteBooking.as_view(), name="delete_booking"),
+    path("edit/<booking_no>",
+         views.BookingEdit.as_view(),
+         name="edit_booking"
+         ),
+    path("delete/<booking_no>",
+         views.DeleteBooking.as_view(),
+         name="delete_booking"
+         ),
     path("all/", views.ShowAllBookings.as_view(), name="all_bookings"),
     path("check/", views.CheckBookings.as_view(), name="check_bookings"),
 ]
