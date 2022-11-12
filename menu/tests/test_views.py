45aaaecd749bab054538b27ec1f9a5a73c754d39
Mattias-b-Pettersson <mattep3@gmail.com>
2022-11-12 00:00:44 +0000

    Add test view set up for menu

---
+++ menu/tests/test_views.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.models import Group, Permission, User
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.shortcuts import get_object_or_404
+from menu.models import (
+    FoodItem,
+    DrinkItem
+)
+import requests
+
+
+class TestMenuViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        self.food_item_1 = FoodItem.objects.create(
+            name="Meatballs",
+            description="Meatballs and spaghetti",
+            price="2.50",
+            type="warm"
+        )
+
+        self.drink_item_1 = DrinkItem.objects.create(
+            name="Cola",
+            description="Cola Zero",
+            price="2.50",
+            type="cold"
+        )
+        self.food_filter = FoodItem.objects.all()
+        self.drink_filter = DrinkItem.objects.all()
+
+        # create permissions group
+        self.user = User.objects.create_user(username="test", email="test@test.com", password="test")
+        self.user.is_active = True
+        self.user.save()
+
+        # URLS
+        self.menu_url = reverse('menu')
+        self.edit_menu_item_url = reverse(
+            "edit_menu_item", args=[self.food_filter.first().slug]
+            )
+        self.delete_food_item_url = reverse(
+            "delete_food_item", args=[self.food_filter.first().slug]
+            )
+        self.delete_food_item_url = reverse(
+            "delete_drink_item", args=[self.drink_filter.first().slug]
+            )
+        self.add_food_item_url = reverse("add_food_item")
+        self.add_drink_item_url = reverse("add_drink_item")
+
+    
+
+    
