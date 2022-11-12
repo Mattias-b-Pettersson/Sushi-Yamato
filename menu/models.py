e7827b01e58b63982ca8e7bf850621e0493cdc5e
Mattias-b-Pettersson <mattep3@gmail.com>
2022-11-12 00:30:14 +0000

    Refactor Menu model

--- menu/models.py
+++ menu/models.py
@@ -3,42 +3,28 @@ from django.utils.text import slugify
 
 # Create your models here.
 
-FOOD_TYPES = (
+MENU_TYPES = (
         ('rolls', 'rolls'),
         ('sushi', 'sushi'),
-        ('warm', 'warm'),
+        ('warm food', 'warm food'),
         ('bowls', 'bowls'),
-)
-
-DRINK_TYPES = (
-        ('warm', 'warm'),
-        ('cold', 'cold'),
+        ('warm beverage', 'warm beverage'),
+        ('cold beverage', 'cold beverage'),
         ('beer', 'beer'),
         ('wine', 'wine'),
         ('sake', 'sake'),
 )
 
 
-class FoodItem(models.Model):
+
+class MenuItem(models.Model):
         name = models.CharField(max_length=100, unique=True)
         slug = models.SlugField(blank=True, null=True)
         description = models.CharField(max_length=300)
         price = models.CharField(max_length=50)
-        type = models.CharField(max_length=100, choices=FOOD_TYPES)
+        type = models.CharField(max_length=100, choices=MENU_TYPES)
         
         def save(self, *args, **kwargs):
                 self.slug = slugify(self.name)
                 super().save(*args, **kwargs)
 
-
-class DrinkItem(models.Model):
-        name = models.CharField(max_length=100, unique=True)
-        slug = models.SlugField(blank=True, null=True)
-        description = models.CharField(max_length=300)
-        price = models.CharField(max_length=50)
-        type = models.CharField(max_length=100, choices=DRINK_TYPES)
-
-        def save(self, *args, **kwargs):
-                self.slug = slugify(self.name)
-                super().save(*args, **kwargs)
-        
