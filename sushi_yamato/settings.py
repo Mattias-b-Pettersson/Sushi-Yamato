ec19f0d5c52d518b8a2bb662d0843614b3734161
Mattias-b-Pettersson <mattep3@gmail.com>
2022-11-15 15:38:57 +0000

    Remove outcommented database in settings

--- sushi_yamato/settings.py
+++ sushi_yamato/settings.py
@@ -101,13 +101,6 @@ WSGI_APPLICATION = 'sushi_yamato.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# Database used for testing enviorment only!
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 # Database for live enviorment
 DATABASES = {
